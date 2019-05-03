d305ea5cb912bc850fe5a00b106374aaf033f46d
Teknical Mage <arcazjordan@gmail.com>
2019-05-03 01:48:54 -0400

    UI base
    UI base

--- untitled/SARAH/SarahUI.py
+++ untitled/SARAH/SarahUI.py
@@ -1,26 +1,35 @@
 import tkinter as tk
+import random
+import time
 
-class Application(tk.Frame):
-    def __init__(self, master=None):
-        super().__init__(master)
-        self.master = master
-        self.pack()
-        self.create_widgets()
+window = tk.Tk()
 
-    def create_widgets(self):
-        self.hi_there = tk.Button(self)
-        self.hi_there["text"] = "Hello World\n(click me)"
-        self.hi_there["command"] = self.say_hi
-        self.hi_there.pack(side="top")
+window.title("S.A.R.A.H")
 
-        self.quit = tk.Button(self, text="QUIT", fg="red",
-                              command=self.master.destroy)
-        self.quit.pack(side="bottom")
+window.geometry("500x500")
 
-    def say_hi(self):
-        print("hi there, everyone!")
+def updaterofsorts():
+    rand = random.randint(1,4)
+    x = rand
+    return(x)
 
-root = tk.Tk()
-app = Application(master=root)
-app.mainloop()
+def updatershow():
+    print("test")
+    greeting = updaterofsorts()
+    n_o_display = tk.Text(master=window, height=10,width=30)
+    n_o_display.grid(column=0, row = 3)
+    n_o_display.insert(tk.END,(greeting))
+
+image = tk.PhotoImage(file="volume2.png")
+label = tk.Label(image=image, height=100, width =100)
+label.grid(column=3,row=3)
+
+title = tk.Label(text ="Kill me")
+title.grid(column = 0, row = 0)
+
+button1 = tk.Button(text="Click me", command=updatershow)
+button1.grid(column = 0, row = 1)
+
+
+window.mainloop()
 
