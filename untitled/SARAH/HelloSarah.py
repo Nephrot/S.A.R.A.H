88b3cabd7f0d270277cac3cf1047bc98af7ffd50
qadada <krimsonmc@gmail.com>
2019-05-19 21:54:32 -0400

    UITest
    t

--- untitled/SARAH/HelloSarah.py
+++ untitled/SARAH/HelloSarah.py
@@ -335,6 +335,95 @@ def getMicOutput(recognizer, microphone):
 def operator():
     print("Operator")
 
+def defineWord(stringStarter): 
+    i = 0
+    while i < stringStarter.__len__():
+        print(i)
+        if stringStarter[i] != "Define":
+            if stringStarter[i] != "the":
+                if stringStarter[i] != "word":
+                    if stringStarter[i] != "of":
+                        if stringStarter[i] != "Definition":
+                            if stringStarter[i] != "Meaning":
+                                    print(stringStarter[i])
+                                    url = stringStarter[i]
+                                    html = requests.get("https://www.vocabulary.com/dictionary/" + url).content
+                                    unicode_str = html.decode("utf-8")
+                                    encoded_str = unicode_str.encode("ascii", "ignore")
+                                    new_soup = BeautifulSoup(encoded_str, "html.parser")
+                                    a_text = new_soup.find_all('p')
+                                    string = str(a_text)
+                                    string = string.replace('<p sclass="one-click-content css-1o84u9 e15kc6du8">', "")
+                                    string = string.replace('<span class="italic">', '')
+                                    string = string.replace('</span>', '')
+                                    string = string.replace('</p>', '')
+                                    string = string.replace('<p>', '')
+                                    string = string.replace('[', '')
+
+                                    string = string.replace('<i>', '')
+                                    string = string.replace('</i>', '')
+
+                                    num2 = string.find('"long')
+                                    string = string[0:num2]
+
+                                    string = string.replace('<p class="short">', '')
+                                    string = string.replace('<p class=', '')
+                                    i = 100000000
+                            else:
+                                i+=1
+                        else:
+                            i+=1
+                    else:
+                        i+=1
+                else:
+                    i+=1
+            else:
+              i+=1
+        else:
+            i+=1
+
+
+    print(string)
+    if string.find("Whether you're a student") == -1:
+        engine = pyttsx3.init()
+        engine.setProperty('voice',"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_ZIRA_11.0")       
+        engine.say(string)
+        engine.runAndWait()
+window = tk.Tk()
+
+
+window.title("S.A.R.A.H")
+
+window.geometry("1920x1080")
+window.attributes("-fullscreen", True)
+
+
+image = tk.PhotoImage(file="C:\\Users\\qadada\\Videos\\Hibernation.png")
+smaller_image = image.subsample(5, 5)  
+panel = tk.Label(window, image = smaller_image)
+
+
+panel.pack(side = "bottom", fill = "both", expand = "yes")
+panel.configure(background="black")
+
+def callback():  
+    img2 = tk.PhotoImage(file="C:\\Users\\qadada\\Videos\\Talking.png")
+    smaller_image = img2.subsample(5, 5)  
+    panel.configure(background = "black", image=smaller_image)
+    panel.image = smaller_image
+
+def callback2():  
+    img2 = tk.PhotoImage(file="C:\\Users\\qadada\\Videos\\Online.png")
+    smaller_image = img2.subsample(5, 5)  
+    panel.configure(background = "black", image=smaller_image)
+    panel.image = smaller_image 
+
+def callback3():  
+    img2 = tk.PhotoImage(file="C:\\Users\\qadada\\Videos\\Hibernation.png")
+    smaller_image = img2.subsample(5, 5)  
+    panel.configure(background = "black", image=smaller_image)
+    panel.image = smaller_image 
+    
 if __name__ == "__main__":   
     window = tk.Tk() 
     window.title("S.A.R.A.H")
